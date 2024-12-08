4ca94dd2e6174c00be6787d5a4e9748a8727301f
Oleksandr Shchur <131793496+shchursasha1@users.noreply.github.com>
2024-12-08 17:30:56 +0200

    minor changes in code structure

--- tests/jxsl_cross_test.cpp
+++ tests/jxsl_cross_test.cpp
@@ -1,104 +1,110 @@
-#include "D:/Programming 2 course/Project/JXSL_CPP/jxsl_lib_cpp.h"
-// include C++ implementation
+// File for cross-testing the C and C++ implementations
+
+#include "jxsl_lib_cpp.h"
+// Include C implementation for cross testing
 extern "C" {
-#include "D:/Programming 2 course/Project/JXSL_C/jxsl_lib.h"
-}   // include C implementation
+#include "JXSL_C/jxsl_lib.h"
+}
 #include <iostream>
 #include <string>
-#include <cstring>
+#include <iomanip>
 
-void compare_add(const std::string& filename, const std::string& key, const std::string& value);
-void compare_edit(const std::string& filename, const std::string& key, const std::string& new_value);
-void compare_delete(const std::string& filename, const std::string& key);
-void compare_read(const std::string& filename, const std::string& key);
+// Function declarations
+void testAdd(const std::string& filename, const std::string& key, const std::string& value);
+void testEdit(const std::string& filename, const std::string& key, const std::string& newValue);
+void testDelete(const std::string& filename, const std::string& key);
+void testRead(const std::string& filename, const std::string& key);
+
+void displayHeader(const std::string& testName);
 
 int main() {
-    // Test file names
-    const std::string json_file = "test.json";
-    const std::string xml_file = "test.xml";
+    const std::string jsonFile = "test.json";
+    const std::string xmlFile = "test.xml";
 
-    // JSON Tests
-    std::cout << "Testing JSON...\n";
-    compare_add(json_file, "key1", "value1");
-    compare_edit(json_file, "key1", "new_value1");
-    compare_read(json_file, "key1");
-    compare_delete(json_file, "key1");
+    // Run tests for JSON
+    displayHeader("Testing JSON");
+    testAdd(jsonFile, "key1", "value1");
+    testEdit(jsonFile, "key1", "new_value1");
+    testRead(jsonFile, "key1");
+    testDelete(jsonFile, "key1");
 
-    // XML Tests
-    std::cout << "\nTesting XML...\n";
-    compare_add(xml_file, "key2", "value2");
-    compare_edit(xml_file, "key2", "new_value2");
-    compare_read(xml_file, "key2");
-    compare_delete(xml_file, "key2");
+    // Run tests for XML
+    displayHeader("Testing XML");
+    testAdd(xmlFile, "key2", "value2");
+    testEdit(xmlFile, "key2", "new_value2");
+    testRead(xmlFile, "key2");
+    testDelete(xmlFile, "key2");
 
     return 0;
 }
 
-// Compare add functionality
-void compare_add(const std::string& filename, const std::string& key, const std::string& value) {
-    JXSL cpp_handler(filename);
-    bool cpp_result = cpp_handler.add_data(key, value);
+// Test add functionality
+void testAdd(const std::string& filename, const std::string& key, const std::string& value) {
+    JXSL cppHandler(filename);
+    bool cppResult = cppHandler.addData(key, value);
 
-    bool c_result;
-    if (filename.find(".json") != std::string::npos) {
-        c_result = add_data_json(filename.c_str(), key.c_str(), value.c_str());
-    } else {
-        c_result = add_data_xml(filename.c_str(), key.c_str(), value.c_str());
-    }
+    bool cResult = (filename.find(".json") != std::string::npos)
+                   ? add_data_json(filename.c_str(), key.c_str(), value.c_str())
+                   : add_data_xml(filename.c_str(), key.c_str(), value.c_str());
 
-    std::cout << "[Add] Key: " << key << ", Value: " << value
-              << "\nC++ Result: " << cpp_result
-              << ", C Result: " << c_result
-              << (cpp_result == c_result ? " [PASS]" : " [FAIL]") << "\n";
+    std::cout << std::left << std::setw(10) << "[Add]"
+              << "Key: " << key << ", Value: " << value
+              << "\n    C++ Result: " << cppResult
+              << ", C Result: " << cResult
+              << (cppResult == cResult ? " [PASS]" : " [FAIL]") << "\n";
 }
 
-// Compare edit functionality
-void compare_edit(const std::string& filename, const std::string& key, const std::string& new_value) {
-    JXSL cpp_handler(filename);
-    bool cpp_result = cpp_handler.edit_data(key, new_value);
+// Test edit functionality
+void testEdit(const std::string& filename, const std::string& key, const std::string& newValue) {
+    JXSL cppHandler(filename);
+    bool cppResult = cppHandler.editData(key, newValue);
 
-    bool c_result;
-    if (filename.find(".json") != std::string::npos) {
-        c_result = edit_data_json(filename.c_str(), key.c_str(), new_value.c_str());
-    } else {
-        c_result = edit_data_xml(filename.c_str(), key.c_str(), new_value.c_str());
-    }
+    bool cResult = (filename.find(".json") != std::string::npos)
+                   ? edit_data_json(filename.c_str(), key.c_str(), newValue.c_str())
+                   : edit_data_xml(filename.c_str(), key.c_str(), newValue.c_str());
 
-    std::cout << "[Edit] Key: " << key << ", New Value: " << new_value
-              << "\nC++ Result: " << cpp_result
-              << ", C Result: " << c_result
-              << (cpp_result == c_result ? " [PASS]" : " [FAIL]") << "\n";
+    std::cout << std::left << std::setw(10) << "[Edit]"
+              << "Key: " << key << ", New Value: " << newValue
+              << "\n    C++ Result: " << cppResult
+              << ", C Result: " << cResult
+              << (cppResult == cResult ? " [PASS]" : " [FAIL]") << "\n";
 }
 
-// Compare delete functionality
-void compare_delete(const std::string& filename, const std::string& key) {
-    JXSL cpp_handler(filename);
-    bool cpp_result = cpp_handler.delete_data(key);
+// Test delete functionality
+void testDelete(const std::string& filename, const std::string& key) {
+    JXSL cppHandler(filename);
+    bool cppResult = cppHandler.deleteData(key);
 
-    bool c_result;
-    if (filename.find(".json") != std::string::npos) {
-        c_result = delete_data_json(filename.c_str(), key.c_str());
-    } else {
-        c_result = delete_data_xml(filename.c_str(), key.c_str());
-    }
+    bool cResult = (filename.find(".json") != std::string::npos)
+                   ? delete_data_json(filename.c_str(), key.c_str())
+                   : delete_data_xml(filename.c_str(), key.c_str());
 
-    std::cout << "[Delete] Key: " << key
-              << "\nC++ Result: " << cpp_result
-              << ", C Result: " << c_result
-              << (cpp_result == c_result ? " [PASS]" : " [FAIL]") << "\n";
+    std::cout << std::left << std::setw(10) << "[Delete]"
+              << "Key: " << key
+              << "\n    C++ Result: " << cppResult
+              << ", C Result: " << cResult
+              << (cppResult == cResult ? " [PASS]" : " [FAIL]") << "\n";
 }
 
-// Compare read functionality
-void compare_read(const std::string& filename, const std::string& key) {
-    JXSL cpp_handler(filename);
-    std::string cpp_value;
-    bool cpp_result = cpp_handler.read_data(key, cpp_value);
+// Test read functionality
+void testRead(const std::string& filename, const std::string& key) {
+    JXSL cppHandler(filename);
+    std::string cppValue;
+    bool cppResult = cppHandler.readData(key, cppValue);
 
-    char c_value[256] = {0};
-    bool c_result = read_data(filename.c_str(), key.c_str(), c_value);
+    char cValue[256] = {};
+    bool cResult = read_data(filename.c_str(), key.c_str(), cValue);
 
-    std::cout << "[Read] Key: " << key
-              << "\nC++ Result: " << cpp_result << ", Value: " << cpp_value
-              << "\nC Result: " << c_result << ", Value: " << c_value
-              << (cpp_result == c_result && cpp_value == c_value ? " [PASS]" : " [FAIL]") << "\n";
+    std::cout << std::left << std::setw(10) << "[Read]"
+              << "Key: " << key
+              << "\n    C++ Result: " << cppResult << ", Value: " << cppValue
+              << "\n    C Result: " << cResult << ", Value: " << cValue
+              << (cppResult == cResult && cppValue == cValue ? " [PASS]" : " [FAIL]") << "\n";
+}
+
+// Display a formatted test section header
+void displayHeader(const std::string& testName) {
+    std::cout << "\n" << std::string(40, '=') << "\n";
+    std::cout << testName << "\n";
+    std::cout << std::string(40, '=') << "\n";
 }
